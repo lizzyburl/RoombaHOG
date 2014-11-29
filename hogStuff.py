f7e3ed9d03c5b5ab9aa468d40a021b03aa2e7d45
lizzyburl <lizzyburl@gmail.com>
2014-11-29 16:56:07 -0500

    First Code commit

---
+++ hogStuff.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+import slitUpBlocks
+
+
+img = np.double(cv2.imread('person.jpg', cv2.CV_LOAD_IMAGE_GRAYSCALE))/255.;
+
+height, width = img.shape
+
+cv2.imshow('image',img);
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+# Run each image through the mask in the x and y direction
+# filter2d [-1 0 1]
+kern = np.matrix([-1, 0, 1]);
+
+kernTrans = np.transpose(kern);
+gx = cv2.filter2D(img, -1, kern)
+gy = cv2.filter2D(img, -1, kernTrans)
+
+# Compute gradient fro magnitude and orientation
+# M(x,y) = sqrt(gx^2 + gy^2)
+magnitude = np.sqrt(np.multiply(gx,gx) + np.multiply(gy,gy));
+cv2.imshow('image',magnitude);
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# O(x,y) = tan^-1(gy/gx)
+orientation = np.arctan2(gy, gx);
+cv2.imshow('image', orientation);
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+v = slitUpBlocks.overlappingBlocksHistogram(magnitude, orientation)
+
